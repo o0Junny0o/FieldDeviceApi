843e2ffd6893e993364dbd251513aa22d54181a5
o0Junny0o <jujup1209@gmail.com>
2026-09-09 13:23:38 -0300

    Add project.nuget.cache file for FieldDevice.Api with package dependencies

--- src/FieldDevice.Api/Models/Device.cs
+++ src/FieldDevice.Api/Models/Device.cs
@@ -1,3 +1,4 @@
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace FieldDevice.Api.Models
@@ -5,14 +6,19 @@ namespace FieldDevice.Api.Models
     public enum DeviceStatus { Active, Maintenance, Inactive }
 
     public class Device
-n    {
+    {
         [Key]
         public Guid Id { get; set; }
+
         [Required]
         public string SerialNumber { get; set; } = string.Empty;
+
         public string Model { get; set; } = string.Empty;
+
         public DeviceStatus Status { get; set; } = DeviceStatus.Active;
+
         public string Location { get; set; } = string.Empty;
+
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     }
 }
